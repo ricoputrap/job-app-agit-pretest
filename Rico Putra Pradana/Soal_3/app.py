e454295b6f9dc9be4948e3f0e0d4cdf74c06f0cd
rico.putra <rico.putra@outlook.co.id>
2020-09-04 21:31:06 +0700

    DONE soal_3

---
+++ Rico Putra Pradana/Soal_3/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/soal/pretest/', methods=['GET'])
+def pretest():
+    return jsonify({'nama':'mukidi',
+                    'email':'mukidi@ai.astra.co.id',
+                    'usia':'28 Tahun, 3 Bulan'})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
