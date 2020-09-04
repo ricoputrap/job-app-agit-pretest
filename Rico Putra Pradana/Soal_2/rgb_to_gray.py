6eff72e174e22fc11653b41a0348e97ff92bb338
rico.putra <rico.putra@outlook.co.id>
2020-09-04 20:02:37 +0700

    DONE soal_2

--- Rico Putra Pradana/Soal_2/rgb_to_gray.py
+++ Rico Putra Pradana/Soal_2/rgb_to_gray.py
@@ -1,6 +1,14 @@
 import cv2
 
-img = cv2.imread('hilih.png')
+# read the original image
+img_rgb = cv2.imread('img/hilih.png')
 
-cv2.imshow("Output", img)
+# convert image from rgb to grayscale
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+# save the converted image
+cv2.imwrite('img/hilih-gray.png', img_gray)
+
+# show the converted image
+cv2.imshow("Output", img_gray)
 cv2.waitKey(0)
