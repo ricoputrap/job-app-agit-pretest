14fd8aebe329089a69dc2e60829f80a222b60c35
rico.putra <rico.putra@outlook.co.id>
2020-09-05 23:05:12 +0700

    refactor soal 6

--- Rico Putra Pradana/Soal_6/main.py
+++ Rico Putra Pradana/Soal_6/main.py
@@ -2,19 +2,17 @@ from person import Person
 from save_to_excel import Excel
 from convert_img import ImageConverter
 
-print('Selamat datang di Program Terakhir')
-
 def print_main_menu():
     print('Silahkan pilih menu di bawah ini:')
     print('1: Menambahkan Data Diri')
     print('2: Tampilkan Data')
     print('3: Konversi Gambar')
+    print('4: Keluar')
 
 def print_save_menu():
     print('Tujuan penyimpanan:')
-    print('1: simpan sebagai file excel')
-    print('2: simpan ke database')
-    print('3: tidak perlu disimpan, cukup dicetak saja')
+    print('1: Simpan sebagai file excel')
+    print('2: Tidak perlu disimpan, cukup dicetak saja')
 
 def print_user_data(new_person):
     print('Nama Anda : {}'.format(new_person.nama))
@@ -44,12 +42,10 @@ def print_break():
 def print_success_msg(menu):
     print(menu + ' sukses!')
 
-print_main_menu()
-main_menu = input('Masukkan 1, 2, atau 3: ')
-print_break()
+def print_closing_msg():
+    print('Terima kasih telah menggunakan program ini!')
 
-# add new person
-if main_menu == '1':
+def add_new_person():
     print_save_menu()
     save_menu = input('Masukkan 1, 2, atau 3: ')
     print_break()
@@ -58,7 +54,6 @@ if main_menu == '1':
     new_person = Person()
     new_person.get_user_input()
     new_person.calculate_age()
-
     print_break()
 
     # save to excel
@@ -70,39 +65,59 @@ if main_menu == '1':
         new_excel.close_workbook()
         print_success_msg('Menyimpan data ke excel')
 
-    # save to db
+    # print new person data
     elif save_menu == '2':
-        pass
-
-    # print out
-    elif save_menu == '3':
         print_user_data(new_person)
+        print_success_msg('Mencetak data diri')
+        print_break()
+        print_closing_msg()
+
+    # back to save menu
     else:
-        pass # kembali take save_menu
+        print('Mohon maaf Anda memasukkan pilihan yang kurang tepat.')
+        print_break()
+        add_new_person()
 
-# show all data
-elif main_menu == '2':
-    pass 
-
-# open cv
-elif main_menu == '3':
-    print_convert_type()
-    convert_type = input('Masukkan 1 atau 2: ')
-    print_break()
-    
-    print_rotation_type()
-    rotation_type = int(input('Masukkan 1, 2, atau 3: '))
+def open_main_menu():
+    print_main_menu()
+    main_menu = input('Masukkan 1, 2, atau 3: ')
     print_break()
 
-    print_img_selection()
-    selected_img = int(input('Masukkan 1, 2, atau 3: '))
-    
-    img_converter = ImageConverter(selected_img)
+    # add new person
+    if main_menu == '1':
+        add_new_person()
+        
+    # show all data
+    elif main_menu == '2':
+        pass 
 
-    
-    converted_img = img_converter.convert_to_gray() if convert_type == '1' else img_converter.rotate_img(rotation_type)
-    img_converter.save_image(converted_img)
-    img_converter.show_img(converted_img)
+    # convert image
+    elif main_menu == '3':
+        print_convert_type()
+        convert_type = input('Masukkan 1 atau 2: ')
+        print_break()
+        
+        print_rotation_type()
+        rotation_type = int(input('Masukkan 1, 2, atau 3: '))
+        print_break()
 
-else:
-    pass # kembali take main_menu
+        print_img_selection()
+        selected_img = int(input('Masukkan 1, 2, atau 3: '))
+        
+        img_converter = ImageConverter(selected_img)
+        converted_img = img_converter.convert_to_gray() if convert_type == '1' else img_converter.rotate_img(rotation_type)
+        img_converter.save_image(converted_img)
+        img_converter.show_img(converted_img)
+        print_success_msg('Mengonversi gambar')
+
+    elif main_menu == '4':
+        print('Terima kasih telah menggunakan program ini!')
+
+    # back to main menu
+    else:
+        print('Mohon maaf Anda memasukkan pilihan yang kurang tepat.')
+        print_break()
+        open_main_menu()
+
+print('Selamat datang di Program Terakhir')
+open_main_menu()
